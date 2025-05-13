b438c2e0f03dd9c7d6b6b270f6de7e8c79dbc818
jaipaljadeja <jadejajaipal5@gmail.com>
2025-05-14 02:15:37 +0530

    update example

--- apibara.config.ts
+++ apibara.config.ts
@@ -1,3 +1,34 @@
 import { defineConfig } from "apibara/config";
 
-export default defineConfig({});
+export default defineConfig({
+  runtimeConfig: {
+    evm: {
+      startingBlock: 215_30_000,
+    },
+    starknet: {
+      startingBlock: 12_45_000,
+    },
+  },
+  presets: {
+    mainnet: {
+      runtimeConfig: {
+        evm: {
+          startingBlock: 215_30_000,
+        },
+        starknet: {
+          startingBlock: 12_45_000,
+        },
+      },
+    },
+    testnet: {
+      runtimeConfig: {
+        evm: {
+          startingBlock: 80_60_000,
+        },
+        starknet: {
+          startingBlock: 6_60_000,
+        },
+      },
+    },
+  },
+});
